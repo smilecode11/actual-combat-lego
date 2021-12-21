64a08c8bddeaf2a24cf0f940fe7aad3f3b8eedfa
尘心 <smilewupeng@gmail.com>
2021-12-21 11:31:11 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<h1>慕课乐高标准模版</h1>
+	<hello :msg="helloMessage"></hello>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Hello from "./components/Hello";
+
+export default defineComponent({
+	name: "App",
+	setup() {
+		const helloMessage = "hello, Chenxin";
+
+		return {
+			helloMessage
+		};
+	},
+	components: {
+		Hello
+	}
+});
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: ['msg'],
+  setup(props: any) {
+
+    const render = () => {
+      return <>
+        <div>{props.msg}</div>
+      </>
+    }
+    return render
+  }
+})
